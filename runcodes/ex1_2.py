b2363937ddd70ff5308662063216835082a2c25d
lfvperes <luisfvperes@gmail.com>
2026-04-26 11:13:10 -0300

    lista 1 de exercícios no runcodes

--- runcodes/ex1_2.py
+++ runcodes/ex1_2.py
@@ -10,12 +10,13 @@ modelo.hideOutput(True)
 # variaveis de decisao
 x1 = modelo.addVar(vtype="C", lb=0)
 x2 = modelo.addVar(vtype="C", lb=0)
+x3 = modelo.addVar(vtype="C", lb=0)
 # funcao objetivo
-modelo.setObjective(1*x1 + 2*x2, sense="maximize")
+modelo.setObjective(0.56*x1+0.81*x2+0.46*x3, sense="minimize")
 # restricoes
-modelo.addCons(x1+x2 <= 4)
-modelo.addCons(x1 <= 2)
-modelo.addCons(x2 <= 3)
+modelo.addCons(0.2*x1 + 0.5*x2 + 0.4*x3 >= 0.3)
+modelo.addCons(0.6*x1 + 0.4*x2 + 0.4*x3 >= 0.5)
+modelo.addCons(x1 + x2 + x3 == 1)
 # resolver problema
 modelo.optimize()
 # mostrar resultado
